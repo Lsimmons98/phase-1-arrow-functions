c18353e214e94bbb0f992cc59f800897bbb01806
Liam Simmons <liam.simmons@powerhrg.com>
2024-05-07 08:30:56 -0400

    Add arrow functions to index file

---
+++ index.js
@@ -0,0 +1,5 @@
+const divide = (x = 2000) => x / 100
+const square = x => x * x;
+const add = (a, b) => a + b;
+
+// For the divide function I originally had const divide = 2000/100. It wasn't passing the test "1) divide divides 2000 by 100." Not sure why it wouldn't pass but what I put in was the only way I could get it to work. 
